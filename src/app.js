3463771cf64c53d4a9630861d2ce7eb614a57a6d
srilekha2452 <srilekha3890@gmail.com>
2025-07-13 22:25:07 +0530

    writin auth-middlewear

--- src/app.js
+++ src/app.js
@@ -1,30 +1,14 @@
 const express=require('express');
+const { adminAuth } = require('./middlewares/auth');
 const app=express();
 
-app.use('/user', 
-    (req,res,next)=>{
-        console.log('this is my first route handler');
-        //res.send('this is first argument in the use method');
-        next();
-    },
-        
-    (req,res,next)=>{
-        console.log('this is my second route handler');
-        next();
-    },
-    (req,res,next)=>{
-        console.log('this is my third route handler');
-        next();
-    },
-    (req,res,next)=>{
-        console.log('this is my fourth route handler');
-        next();
-    },
-    (req,res)=>{
-        res.send('this is my fifth route handler')
-    }
-
-)
+app.use('/admin',adminAuth)
+app.get('/admin/getAllData',(req,res)=>{
+    res.send('All data sent')
+});
+app.get('admin/deleteUser',(req,res)=>{
+    res.send("delete a user")
+})
 
 app.listen(3000, ()=>{
     console.log('server is successfully listening on port 3000...')
---
+++ src/middlewares/auth.js
@@ -0,0 +1,13 @@
+const adminAuth=(req,res,next)=>{
+const token='xyz';
+const isAdminAuthorized=token==='xyz';
+if(!isAdminAuthorized){
+    res.status(401).send('unauthorized request');
+}
+else{
+    next();
+}
+};
+module.exports={
+    adminAuth
+}
