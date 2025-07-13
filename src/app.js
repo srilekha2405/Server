1d6cd22261c524c5fa54f3fef844561e536a7b99
srilekha2452 <srilekha3890@gmail.com>
2025-07-13 17:00:10 +0530

    exploring code with next method

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
 const express=require('express');
 const app=express();
-app.use('/hello',(req,res)=>{
-    res.send('hello world');
-})
+
+app.use('/user', 
+    (req,res,next)=>{
+        console.log('this is my first route handler');
+        //res.send('this is first argument in the use method');
+        next();
+    },
+        
+    (req,res,next)=>{
+        console.log('this is my second route handler');
+        next();
+    },
+    (req,res,next)=>{
+        console.log('this is my third route handler');
+        next();
+    },
+    (req,res,next)=>{
+        console.log('this is my fourth route handler');
+        next();
+    },
+    (req,res)=>{
+        res.send('this is my fifth route handler')
+    }
+
+)
+
 app.listen(3000, ()=>{
     console.log('server is successfully listening on port 3000...')
 })
